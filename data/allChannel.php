d6ceded58c7cd69359d39e440fccb81dd69066c4
LeHieu <0043113@gmail.com>
2014-06-26 22:39:04 +0700

    init

--- data/allChannel.php
+++ data/allChannel.php
@@ -1,38 +1,18 @@
 
 <?php
 $sql = $con->prepare("Select * from `channel`");
-$sql->execute(array());
-$content1='';
-$content2='';
-$content3='';
-$content4='';
-$content5='';
-$content6='';
+$sql->execute();
 
-foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $rows){
+
+foreach($sql->fetchAll() as $rows){
 if($rows['alt']=='') $alt = $rows['title'];
 else $alt = $rows['alt'];
-$content[$rows['groups']] .= '<figure class="channel"><a title="'.$rows['title'].'"  href="'.$rows['url'].'"><img class="imgchl" src="'.$rows['imgsrc'].'" alt="'.$alt.'"/></a><div class="popup" rel=''></div></figure>'
+$content1 .= '<figure class="channel"><a title="'.$rows['title'].'"  href="'.$rows['url'].'"><img class="imgchl" src="'.$rows['imgsrc'].'" alt="'.$alt.'"/></a><div class="popup" rel=''></div></figure>'
 }
 
 ?>
 
 
 <div class="content1">
-<?=$content[1];?>
-</div>
-<div class="content2">
-<?=$content[2];?>
-</div>
-<div class="content3">
-<?=$content[3];?>
-</div>
-<div class="content4">
-<?=$content[4];?>
-</div>
-<div class="content5">
-<?=$content[5];?>
-</div>
-<div class="content6">
-<?=$content[6];?>
+<?=$content1;?>
 </div>
