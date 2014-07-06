46b84670a893985e7eba9eab0a8ec67cb0fd5f4b
LeHieu <0043113@gmail.com>
2014-07-06 23:30:59 +0700

    init

--- data/ListChannel.php
+++ data/ListChannel.php
@@ -5,7 +5,7 @@ $query = $sql->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <style>
 
-.delete{
+#delete{
 display:inline;
 cursor:pointer;
 background-color:#646464;
@@ -15,13 +15,14 @@ padding:5px;
 </style>
 <script>
 $(function(){
-$('.delete').click(function(){
+$('#delete').click(function(){
 $.post("/delChannel.php", {id:$(this).attr('idmv')}, function(suc){
 if(suc.success==true){
 $('.notice').css({'right':10});
 $('.notice').text('Đã xóa thành công!');
 setTimeout(function(){$('.notice').css({'right':-500});}, 2000);
-$(this).parent().remove();
+$(this).parent('tr').remove();
+$(this).parent('tr').hide();
 }else{
 $('.notice').css({'right':10});
 $('.notice').text('Lỗi không thể xóa được!');
@@ -49,7 +50,7 @@ foreach($query as $rows){
 <tr>
 <td class="tdname"><div class="maxcontent"><?=$rows['url']?></div>
 <td class="function"><div class="maxcontent"><a href="/editChannel/<?=$rows['id']?>">Sửa Thông tin</a></div>
-<td class="delete" idmv="<?=$rows['id']?>">Xóa
+<td id="delete" idmv="<?=$rows['id']?>">Xóa
 <?php
 }
 ?>
