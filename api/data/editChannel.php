88b4279f68e1ddf4167e8be6c432e12bb814284f
Le <0043113@gmail.com>
2014-07-30 11:14:15 +0700

    init

--- api/data/editChannel.php
+++ api/data/editChannel.php
@@ -7,18 +7,68 @@ header('Location: /');
 $sql = $con->prepare("SELECT * FROM `apichannel` WHERE id=?");
 $sql->execute(array($extend1));
 $row =$sql->fetch(PDO::FETCH_ASSOC);
+$querysv = $con->prepare("SELECT * FROM `server` WHERE idchannel=?");
+$querysv->execute(array($extend1));
 ?>
+<script>
+$(function(){
+$('.themtapphim').click(function(){
+var crEp = $("#soserver").attr("value");
+var nextEp = parseInt(crEp)+1;
+$('#server').append("<div class='content'>Server "+nextEp+":<br><input type='text' class='classinput' name='server["+nextEp+"]' placeholder='Link ...'><input class='mininput' type='text' name='type["+nextEp+"]' placeholder='Kiểu link....'><input class='mininput' type='text' name='device["+nextEp+"]' placeholder='Thiết bị....'><input type='button' class='delete' sequence="+nextEp+" value='Xoá'></div>");
+$("#soserver").attr("value", nextEp);
+});
+$('.delete').click(function(){
+$(this).parent().remove();
+var i = parseInt($("#soserver").val());
+$("#soserver").val(i-1);
+var str = $("#serverdeleted").val();
+str += ","+$(this).attr("sequence");
+});
+});
+</script>
 <form name="up2" method="post">
 Tên kênh:
 <input class="classinput" type="text" name="name" placeholder="Tên kênh..." value="<?=$row['name']?>">
+Tiêu đề:
+<input class="classinput" type="text" name="genre" placeholder="Tiêu đề...." value="<?=$row['genre']?>">
 Link Ảnh:
 <input class="classinput" type="text" name="img" placeholder="Link ảnh...." value="<?=$row['imgsrc']?>">
-Số Server:
-<input class="classinput" type="text" name="numsv" placeholder="Số Server...." value="<?=$row['numsv']?>">
 Nhóm:
 <input class="classinput" type="text" name="groups" placeholder="Nhóm...." value="<?=$row['groups']?>">
+<section id="server">
+<?php
+$i=0;
+for($querysv->fetch(PDO::FETCH_ASSOC) as rowssv){
+$i++;
+?>
+<div class="content">
+Server <?=$rowssv['server']?>:
+<input class="classinput" type="text" name="url[<?=$i?>]" value="<?=$rowssv['url']?>" placeholder="Link Server....">
+<input class="mininput" type="text" name="type[<?=$i?>]" value="<?=$rowssv['type']?>" placeholder="Kiểu link....">
+<input class="mininput" type="text" name="device[<?=$i?>]" value="<?=$rowssv['device']?>" placeholder="Thiết bị....">
+<input type="button" class="delete" sequence=<?=$i?> value="Xoá">
+</div>
+<?php
+}
+?>
+</section>
+<div class="themtapphim">Thêm server</div>
+<input type="hidden" name="soserver" id="soserver" value="<?=$row['numsv']?>">
+<input type="hidden" name="serverdeleted" id="serverdeleted" value="">
 <input class="button" type="submit" name="submit" value="Gửi">
 </form>
+<div class="info">
+<em style="padding-left:100px">1. M3U8, RTMP<br>
+2. HTV, VTVplus, VTVPlay<br>
+3. FPT<br>
+4. VNPT<br>
+5. Clip<br>
+Thiết bị:<br>
+0. Tất cả<br>
+1. PC
+</em>
+</div>
 <?php
 }
 ?>
