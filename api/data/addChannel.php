4772b0c42f0556a2bf38da41a44c78be89156884
Le <0043113@gmail.com>
2014-07-30 11:00:04 +0700

    Init

--- api/data/addChannel.php
+++ api/data/addChannel.php
@@ -4,14 +4,18 @@ $sql = $con->prepare("SELECT MAX(sort) FROM `apichannel` where `groups`=?");
 $sql->execute(array($_POST['groups']));
 $row = $sql->fetch(PDO::FETCH_ASSOC);
 $next_sort = $row['MAX(sort)']+10;
-$query = $con->prepare("INSERT INTO `apichannel`(`name`,`imgsrc`,`numsv`,`groups`,`sort`) VALUES (:name,:img,:numsv,:groups,:sort)");
-$query->execute(array(":name"=> $_POST['name'],":img"=> $_POST['img'],":numsv"=> $_POST['numsv'],":groups"=> $_POST['groups'],":sort"=>$next_sort));
+$query = $con->prepare("INSERT INTO `apichannel`(`name`,`genre`,`imgsrc`,`numsv`,`groups`,`sort`) VALUES (:name,:genre:img,:numsv,:groups,:sort)");
+$query->execute(array(":name"=> $_POST['name'],":genre"=>$_POST['genre'],":img"=> $_POST['img'],":numsv"=> $_POST['numsv'],":groups"=> $_POST['groups'],":sort"=>$next_sort));
 $idchannel = $query->lastInsertId();
+$sv=0;
 for($i=1; $i<=$_POST['soserver']; $i++){
+if(!strpos($i, $_POST["serverdeleted"])){
+$sv++;
 $server = $con->prepare("INSERT INTO `server`(`idchannel`, `server`, `url`, `type`, `device`) VALUES (:idchannel, :server, :url, :type, :device)");
-$server->execute(array(":idchannel"=> $idchannel, ":server"=>$i, ":url"=>$_POST['url'][$i], ":type"=>$_POST['type'][$i], ":device"=>$_POST['device'][$i]));
+$server->execute(array(":idchannel"=> $idchannel, ":server"=>$sv, ":url"=>$_POST['url'][$i], ":type"=>$_POST['type'][$i], ":device"=>$_POST['device'][$i]));
 }
-header("Location: /");
+}
+header("Location: /api/");
 }else{
 ?>
 <script>
@@ -19,13 +23,15 @@ $(function(){
 $('.themtapphim').click(function(){
 var crEp = $("#soserver").attr("value");
 var nextEp = parseInt(crEp)+1;
-$('#server').append("<div class='content'>Server "+nextEp+":<br><input type='text' class='classinput' name='server["+nextEp+"]' placeholder='Link ...'><input class='mininput' type='text' name='type["+nextEp+"]' placeholder='Kiểu link....'><input class='mininput' type='text' name='device["+nextEp+"]' placeholder='Thiết bị....'><input type='button' class='delete' value='Xoá'></div>");
+$('#server').append("<div class='content'>Server "+nextEp+":<br><input type='text' class='classinput' name='server["+nextEp+"]' placeholder='Link ...'><input class='mininput' type='text' name='type["+nextEp+"]' placeholder='Kiểu link....'><input class='mininput' type='text' name='device["+nextEp+"]' placeholder='Thiết bị....'><input type='button' class='delete' sequence="+nextEp+" value='Xoá'></div>");
 $("#soserver").attr("value", nextEp);
 });
 $('.delete').click(function(){
 $(this).parent().remove();
 var i = parseInt($("#soserver").val());
 $("#soserver").val(i-1);
+var str = $("#serverdeleted").val();
+str += ","+$(this).attr("sequence");
 });
 });
 </script>
@@ -34,7 +40,7 @@ $("#soserver").val(i-1);
 Tên kênh:
 <input class="classinput" type="text" name="name" placeholder="Tên kênh...">
 Tiêu đề:
-<input class="classinput" type="text" name="name" placeholder="Tiêu đề...">
+<input class="classinput" type="text" name="genre" placeholder="Tiêu đề...">
 Link Ảnh:
 <input class="classinput" type="text" name="img" placeholder="Link ảnh....">
 Số Server:
@@ -47,11 +53,12 @@ Server 1:
 <input class="classinput" type="text" name="url[1]" placeholder="Link Server....">
 <input class="mininput" type="text" name="type[1]" placeholder="Kiểu link....">
 <input class="mininput" type="text" name="device[1]" placeholder="Thiết bị....">
-<input type="button" class="delete" value="Xoá">
+<input type="button" class="delete" sequence=1 value="Xoá">
 </div>
 </section>
 <div class="themtapphim">Thêm server</div>
 <input type="hidden" name="soserver" id="soserver" value="1">
+<input type="hidden" name="serverdeleted" id="serverdeleted" value="">
 <input class="button" type="submit" name="submit" value="Gửi">
 </form>
 <div class="info">
