5de8840d0589e29a5e46c630abbaacf105c9f1e8
Le <0043113@gmail.com>
2014-07-30 09:16:11 +0700

    Init

--- api/data/addChannel.php
+++ api/data/addChannel.php
@@ -14,7 +14,7 @@ $(function(){
 $('.themtapphim').click(function(){
 var crEp = $("#soserver").attr("value");
 var nextEp = parseInt(crEp)+1;
-$('.server').append("Tập "+nextEp+":<br><input type='text' class='classinput' name='server["+nextEp+"]' placeholder='Mã phim ...'>");
+$('.server').append("Tập "+nextEp+":<br><input type='text' class='classinput' name='server["+nextEp+"]' placeholder='Mã phim ...'><input class='classinput' type='text' name='type[1]' placeholder='Kiểu link....'><input class='classinput' type='text' name='device[1]' placeholder='Thiết bị....'>");
 $("#soserver").attr("value", nextEp);
 });
 });
@@ -34,12 +34,21 @@ Nhóm:
 <section id="server">
 Server 1:
 <input class="classinput" type="text" name="server[1]" placeholder="Link Server....">
-<input class="classinput" type="text" name="server[1]" placeholder="Link Server....">
+<input class="classinput" type="text" name="type[1]" placeholder="Kiểu link....">
+<input class="classinput" type="text" name="device[1]" placeholder="Thiết bị....">
 <div class="themtapphim">Thêm server</div>
 <input type="hidden" name="soserver" id="soserver" value="1">
 </section>
 <input class="button" type="submit" name="submit" value="Gửi">
 </form>
+<div class="info">
+<em>1. M3U8, RTMP<br>
+2. HTV, VTVplus, VTVPlay<br>
+3. FPT<br>
+4. VNPT<br>
+5. Clip<br>
+</em>
+</div>
 <?php
 }
 ?>
