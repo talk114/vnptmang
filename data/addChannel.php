96a762181a6efe7c57e8f9661c89bbb59091471c
Hieu <0043113@gmail.com>
2014-08-20 20:34:07 +0700

    Init

--- data/addChannel.php
+++ data/addChannel.php
@@ -2,7 +2,7 @@
 if(isset($_POST['submit']) && $_POST['url']!=''){
 $sql = $con->prepare("SELECT MAX(sort) FROM `channel` where `groups`=?");
 $sql->execute(array($_POST['groups']));
-$row = $sql_fecth(PDO::FETCH_ASSOC);
+$row = $sql->fecth(PDO::FETCH_ASSOC);
 $next_sort = $row['MAX(sort)']+10;
 $query = $con->prepare("INSERT INTO `channel`(`url`,`title`,`imgsrc`,`alt`,`groups`,`sort`) VALUES (:url,:title,:img,:alt,:groups,:sort)");
 $query->execute(array(":url"=> $_POST['url'],":title"=> $_POST['title'],":img"=> $_POST['img'],":alt"=> $_POST['alt'],":groups"=> $_POST['groups'],":sort"=>$next_sort));
