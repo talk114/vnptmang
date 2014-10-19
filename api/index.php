75130057d12efcff18009cbed87774167da3d795
Hieu <0043113@gmail.com>
2014-10-19 15:03:09 +0700

    Init

--- api/index.php
+++ api/index.php
@@ -31,7 +31,7 @@ API
 if(!isset($_COOKIE['admin'])) header("Location: http://vnptmang.com");
 //if($_SERVER['HTTP_X_FORWARDED_PROTO']!='https') 
 	//header("Location: https://ch.vnptmang.com/api");
-	echo $_SERVER['SERVER_PORT'];
+	echo getenv('HTTP_X_FORWARDED_PROTO');
 include_once('../mysql.php');
 $uri = $_SERVER['REQUEST_URI'];
 list($null, $folder,$index, $fun, $extend1, $extend2, $extend3) = explode('/', $uri);
