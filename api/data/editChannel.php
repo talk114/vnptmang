80184ea55ef4b51e17692dba00a77489eac271a0
Le <0043113@gmail.com>
2015-01-30 22:55:53 +0700

    init

--- api/data/editChannel.php
+++ api/data/editChannel.php
@@ -8,7 +8,7 @@ $strdel = str_replace(",]", "", $strdel);
 $del=$con->prepare("DELETE FROM `server` where `id` in (".$strdel.")");
 $del->execute();
 
-for($i=1; $i<=sizeof($_POST['url']); $i++){
+for($i=0; $i<=sizeof($_POST['url']); $i++){
 if($_POST['url'][$i]!=""){
  if($i>$_POST["addedserver"]){
 //Thêm server
