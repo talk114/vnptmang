5bf137c931b779d41804ec49596941bf1969fcc0
Le <0043113@gmail.com>
2015-01-31 00:21:53 +0700

    init

--- api/reloadListChannel.php
+++ api/reloadListChannel.php
@@ -5,11 +5,15 @@ $sql->execute(array());
 $query = $sql->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <?php
+$arr = array("", "Link M3u8", "VNN", "FPT", "VNPT", "Clip", "HTVOnline", "VTVplus", "VTVplay", "Social", "Movie", "TV VNN");
 foreach($query as $rows){
+	$str .= $arr[$row['type']]." ,";
 ?>
 <tr>
 <td class="tdname"><div class="maxcontent"><?=$rows['name']?></div>
-<td class="function"><div class="maxcontent"><a href="/editChannel/<?=$rows['id']?>">Sửa Thông tin</a></div>
+<td class="function"><div class="maxcontent"><a href="/api/index.php/editChannel/<?=$rows['id']?>">Sửa Thông tin</a></div>
+<td class="tdname"><div class="maxcontent">PC: <?=$rows['numsv']?> / Mobi: <?=$rows['numbersv_mobile']?></div>
+<td class="detail"><div class="maxcontent"><?=$str?></div>
 <td class="delete" idmv="<?=$rows['id']?>">Xóa
 <?php
 }
